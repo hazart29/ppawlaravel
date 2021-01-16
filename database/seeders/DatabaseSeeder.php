22ba2abf0f73761dada8d241130dfc7999b81d34
hazart29 <misbakhul2904@gmail.com>
2021-01-16 21:43:41 +0700

    perbaikan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(1)->create();
+        \App\Models\User::create([
+            'name' => 'admin',
+            'username' => 'admin',
+            'password' => Hash::make('admin12345'),
+            'roles' => 'akademik',
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
